25908ca9e88bb9312b9be3c6f1d69762c1b7c8ce
electrovir <electrovir@users.noreply.github.com>
2023-06-04 20:31:56 +0000

    export ShapeMismatchError

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './define-shape/define-shape';
 export * from './define-shape/shape-specifiers';
+export * from './errors/shape-mismatch.error';
 export * from './verify-shape/verify-shape';
