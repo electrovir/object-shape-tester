2745ca1f39afc8b9ad713be747989b0d7c50f47b
electrovir <electrovir@users.noreply.github.com>
2024-11-20 21:11:53 +0000

    [patch] make runtimeType unenumerable

--- src/define-shape/define-shape.ts
+++ src/define-shape/define-shape.ts
@@ -1,19 +1,48 @@
-import {ShapeDefinition, ShapeToRunTimeType, isShapeDefinitionKey} from './shape-specifiers';
+import type {AnyObject} from '@augment-vir/common';
+import {ShapeDefinition, isShapeDefinitionKey} from './shape-specifiers';
 import {shapeToDefaultValue} from './shape-to-default-value';
 
+/**
+ * Creates a {@link ShapeDefinition} from any input. This produces both a type and a default value.
+ * This is the core of the `object-shape-tester` package; {@link ShapeDefinition} instances are used
+ * for all of the shape checking.
+ *
+ * @category Main
+ * @example
+ *     ```ts
+ *     import {defineShape} from 'object-shape-tester';
+ *
+ *     const myShape = defineShape({
+ *         a: '',
+ *         b: -1,
+ *     });
+ *
+ *     function doThing(
+ *         // using the generated type and default value
+ *         input: typeof myShape.runtimeType = myShape.defaultValue,
+ *     ) {}
+ *     ```;
+ */
 export function defineShape<Shape, IsReadonly extends boolean = false>(
     shape: Shape,
     isReadonly: IsReadonly = false as IsReadonly,
 ): ShapeDefinition<Shape, IsReadonly> {
-    return {
+    const shapeDefinition = {
         shape,
-        get runTimeType(): ShapeToRunTimeType<Shape, false, IsReadonly> {
-            throw new Error(`runTimeType cannot be used as a value, it is only for types.`);
-        },
         isReadonly,
         get defaultValue() {
             return shapeToDefaultValue(shape);
         },
         [isShapeDefinitionKey]: true,
     };
+
+    Object.defineProperty(shapeDefinition, 'runTimeType', {
+        enumerable: false,
+        configurable: false,
+        get() {
+            throw new Error(`runtimeType cannot be used as a value, it is only for types.`);
+        },
+    });
+
+    return shapeDefinition as AnyObject as ShapeDefinition<Shape, IsReadonly>;
 }
